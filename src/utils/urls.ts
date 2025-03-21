e4ba505a42d65a30535b64ab71bb05f613903b9d
unknown <aronilie.code@gmail.com>
2025-03-21 23:03:02 +0100

    Updated scripts

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -9,5 +9,5 @@ const whatsProdApiUrl = 'https://apiwa.autotrafic.es';
 const whatsLocalApiUrl = 'http://localhost:3200';
 export const WHATSAPP_API_URL = isWhatsappTest ? whatsLocalApiUrl : whatsProdApiUrl;
 
-export const WHATSAPP_HELP_LINK =
+export const AUTOTRAFIC_WHATSAPP_PUBLIC_URL =
   'https://api.whatsapp.com/send?phone=34643219297&text=Hola%2C%20se%20ha%20producido%20un%20error%20cuando%20intentaba%20subir%20los%20documentos%20del%20veh%C3%ADculo%2C%20%C2%BFpodr%C3%ADas%20ayudarme%3F';
